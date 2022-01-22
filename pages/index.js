1576e5a3acf0d4c9e7795b891fe97cc58c6f90f1
n.tldr <42711253+NiteshTuladhar@users.noreply.github.com>
2022-01-22 18:53:33 +0545

    ini commit

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+import ArticleList from '../components/ArticleList'
+import { server } from '../config'
+
+
+
+export default function Home({ articles }) {
+
+  return (
+    <div>
+      <Head>
+        <title>WebDev Simplified</title>
+        <meta name='keyword' content='web development, programming' />
+      </Head>
+      <ArticleList articles={articles} />
+
+     
+    </div>
+  )
+}
+export const getStaticProps = async () => {
+  const res = await fetch(`${server}/api/articles`)
+  const articles = await res.json()
+
+  return {
+    props:{
+      articles
+    }
+  }
+}
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+//   const articles = await res.json()
+
+//   return {
+//     props:{
+//       articles
+//     }
+//   }
+// }
